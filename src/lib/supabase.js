16833d5b043cc301dd7b3e755e7422c4910e1848
arturooqz72 <arturooqz@gmail.com>
2026-03-07 20:07:36 -0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hobdiyssfsjdwkacgnwv.supabase.co"
-const supabaseKey = "sb_publishable_gjOF8eia7aVEMaTu69amkQ__4SPMpTB"
+const SUPABASE_URL = "https://hobdiyssfsjdwkacgnwv.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_gjOF8eia7aVEMaTu69amkQ__4SPMpTB";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
